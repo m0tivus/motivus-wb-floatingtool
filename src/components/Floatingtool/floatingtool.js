5c433c68ebb86fe0c4512ad312892ecc003ab141
Cristian Huijse <cristian.huijse@gmail.com>
2021-01-20 16:59:05 -0300

    FM/CH: login prop

--- src/components/Floatingtool/floatingtool.js
+++ src/components/Floatingtool/floatingtool.js
@@ -10,6 +10,8 @@ import { Box, Link, Typography } from '@material-ui/core'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import style from './floatingTool.module.css'
 import { useSelector } from 'react-redux'
+import Footer from '../Footer/Footer'
+import Login from '../LogIn/Login'
 
 import TabLinks from '../TabLinks/tablinks'
 
@@ -62,6 +64,8 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
+    position: 'sticky',
+    bottom: '0px',
   },
   divider: {
     backgroundColor: 'white',
@@ -73,6 +77,9 @@ export default function SimpleSlide() {
   const isProcessing = useSelector((state) => state.processing.isProcessing)
   const classes = useStyles()
   const [open, setOpen] = React.useState(false)
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true)
+
+  const logOut = () => setIsLoggedIn(false)
 
   const toggleOpen = React.useCallback(() => {
     setOpen((prev) => !prev)
@@ -101,23 +108,7 @@ export default function SimpleSlide() {
             </CardContent>
 
             <CardActions className={classes.bottomAction}>
-              <Button
-                variant='contained'
-                color='secondary'
-                className={classes.button}
-                startIcon={<AccountCircleIcon />}
-                size='small'
-              >
-                Login
-              </Button>
-              <Typography>Powered by</Typography>
-              <Link
-                href='http://motivus.cl/'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <Typography>Motivus</Typography>
-              </Link>
+              {isLoggedIn ? <Footer logOut={logOut} /> : <Login />}
             </CardActions>
           </Card>
         </Box>
