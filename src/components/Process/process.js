827c330caf0f65ae8bcf4944c91937e958106404
Fernando Mora <f.mora.g90@gmail.com>
2021-02-11 14:44:23 -0300

    SE|RI|FM Truncate user name

--- src/components/Process/process.js
+++ src/components/Process/process.js
@@ -7,6 +7,7 @@ import { Box, Grid, Link } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { useTheme } from '@material-ui/core/styles'
+import _ from 'lodash'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -77,8 +78,6 @@ function ProjectsCarousel() {
           <Typography variant='h2' align='center'>
             Welcome to the beta version of the{' '}
             <span className={classes.revolution}>Motivus Floating Tool.</span>{' '}
-            Here you’ll be able to share your computing power with the world.{' '}
-            <span className={classes.revolutionText}></span>
           </Typography>
         ) : (
           <Typography variant='h2' align='center'>
@@ -112,7 +111,9 @@ function ProjectsCarousel() {
           <Grid item xs={4}>
             <Box display='flex' flexDirection='column' pl={2}>
               <Typography variant='body2'>User:</Typography>
-              <Typography variant='h3'>{user.name}</Typography>
+              <Typography variant='h3'>
+                {_.truncate(user.name, { length: 20 })}
+              </Typography>
               <Typography variant='body2'>Season:</Typography>
               <Typography variant='h3'>
                 {season && season.name ? season.name : 'n/a'}
